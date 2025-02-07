db28dba98545cd29ac6b30074c9120bf18094e1e
ANEESHT123 <aneesht240@example.com>
2025-02-07 13:20:06 +0530

    Cors Fixed

--- main.py
+++ main.py
@@ -113,7 +113,15 @@ def manage_project(project_details):
     return result_dict
 
 app = Flask(__name__)
-CORS(app, resources={r"/*": {"origins": "https://multiagent-projectmanager.onrender.com"}})
+# Allow all origins, methods, and headers
+CORS(app, resources={r"/*": {"origins": "*"}}, supports_credentials=True)
+
+@app.after_request
+def add_cors_headers(response):
+    response.headers['Access-Control-Allow-Origin'] = '*'
+    response.headers['Access-Control-Allow-Methods'] = 'GET, POST, OPTIONS'
+    response.headers['Access-Control-Allow-Headers'] = 'Content-Type, Authorization'
+    return response
 
 
 @app.route('/manage-project', methods=['POST'])
